a6a0b540220f1bca19abdabf7cb6a5c69726fd3c
vanessateo <vanessateo16@gmail.com>
2016-05-28 19:01:18 +0000

    popbottle REPL

--- popbottle.rb
+++ popbottle.rb
@@ -1,11 +1,39 @@
-def recycler(money)
-bottle_number = money / 2 
+require 'byebug'
+
+def recycler
+
+  puts "How much are you spending?"
+  bottle_number = gets.chomp.to_i / 2 
   pop_from_bottle = bottle_number / 2
   pop_from_cap = bottle_number / 4
 
-  total_pops = pop_from_bottle + pop_from_cap
-  puts total_pops
-end
+  redeemable_total_pops = pop_from_bottle + pop_from_cap
+
+  
+  
+  puts "-----------------------"
+  puts "you have purchased #{bottle_number} number of pops "
+  puts " you can redeem #{redeemable_total_pops} pops with #{bottle_number}"
+  puts "total number of pops obtained is #{redeemable_total_pops + bottle_number}"
+  puts "you have"
+
+  puts "-----------------------"
+
+  puts "next customer please"
+  end 
 
 
-recycler(10)
+recycler
+
+
+
+
+
+
+
+#def recycler
+  #set_customers
+  #ask_amount
+  #give_amount
+  #ask_again
+
